6ddff4bc0f421fa6100bf0cee2fae9ea498518fa
alex.Rostishka <a.rostovsky@xsolla.com>
2018-08-27 00:41:37 +0500

    let's work with it

--- public/index.php
+++ public/index.php
@@ -4,7 +4,50 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App();
+$config = [
+    'settings' => [
+        'db' => [
+            'engine' => 'mysql',
+            'host' => 'localhost',
+            'dbname' => 'mvc',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci'
+        ],
+    ],
+];
+
+$app = new \Slim\App($config);
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../views/', []);
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+    //return component
+    return $view;
+};
+
+// Register component on container
+$container['db'] = function ($c) {
+    $db = $c->get('settings')['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['username'], $db['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+
+// $container['NAME'] = function($c) {
+	//if we need some dependencies:
+    // $db = $c->get('db');
+    // $somethingElse = $c->get('nameOfDependency')
+    // return new NAME();
+// };
 
 
 $app->get('/', function (Request $request, Response $response, array $args) {
